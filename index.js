d40c146e5f1ea671c930db24d2cf6883736b5c47
vanRoth95 <sugarrr95soobs@gmail.com>
2025-08-05 11:22:42 +0700

    hello

---
+++ index.js
@@ -0,0 +1,20 @@
+let name="Sokha";
+let greeting="Hello";
+
+let asset={
+    lol: "Hello",
+    ggg: "Sokha",
+};
+console.log(asset.lol, asset.ggg);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tempCodeRunnerFile.javascript
@@ -0,0 +1,4 @@
+let favoriteFoods= ["Amok", "Kuy Teav", "Bai Sach"];
+for(let i=0; i<3; i++){
+    console.log(`I love to eat ${favoriteFoods[i]}`);
+}
